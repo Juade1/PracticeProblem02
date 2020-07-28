f0c91568b5da04e97ae59b8685bfb852c679cf83
Jubril Aderomilehin <Jubril@Jubrils-MacBook-Air.local>
2020-07-28 15:32:30 -0400

    completed

--- src/main/java/com/stayready/Problem02.java
+++ src/main/java/com/stayready/Problem02.java
@@ -1,4 +1,36 @@
 package com.stayready;
 
+import java.util.ArrayList;
+
 public class Problem02 {
+
+    //create an array list that holds values
+    static int findLongestConseSequ(int arr[]) {
+
+        //create an array
+        ArrayList<Integer> Array= new ArrayList<>();
+        int ans =0;
+
+        //Hash all the array elements
+        int n = arr.length;
+        for (int i = 0; i< n; ++i)
+            Array.add(arr[i]);
+
+        //check the sequence from the start
+        for (int i = 0; i < n; ++i) {
+
+            // net element in the sequence
+            if (!Array.contains(arr[i] - 1)) {
+                int j = arr[i];
+                while (Array.contains(j))
+                    j++;
+
+                // update  optimal length if this more
+                if (ans < j - arr[i])
+                    ans = j - arr[i];
+            }
+        }
+        return ans;
+
+    }
 }
--- src/test/java/com/stayready/Problem02Test.java
+++ src/test/java/com/stayready/Problem02Test.java
@@ -1,4 +1,37 @@
 package com.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Problem02Test {
+    @Test
+    public void findLongestConseSequTest(){
+        int arr[] = {10, 12, 11};
+        Problem02 problem02 = new Problem02();
+        String expected = "Length of the longest contiguous subarray is 3";
+        String actual = "Length of the longest contiguous subarray is " + problem02.findLongestConseSequ(arr);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void findLongestConseSequ2Test(){
+        int arr[] = {14, 12, 11, 20};
+        Problem02 problem02 = new Problem02();
+        String expected = "Length of the longest contiguous subarray is 2";
+        String actual = "Length of the longest contiguous subarray is "+ problem02.findLongestConseSequ(arr);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void findLongestConseSequ3Test(){
+        int arr[] = {1, 56, 58, 57, 90, 92, 94, 93, 91, 45};;
+        Problem02 problem02 = new Problem02();
+        String expected = "Length of the longest contiguous subarray is 5";
+        String actual = "Length of the longest contiguous subarray is " + problem02.findLongestConseSequ(arr);
+        Assert.assertEquals(expected,actual);
+    }
+
 }
